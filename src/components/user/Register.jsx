9cba173c786117c4d0369d7161fb9e06572748aa
saikarthik-0109 <saikarthikthatikonda0109gmail.com>
2025-06-19 13:57:34 +0530

    made Responsive Registration page

--- src/components/user/Register.jsx
+++ src/components/user/Register.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CgNametag } from "react-icons/cg";
 import { FaRegUser } from "react-icons/fa";
 import { MdOutlineMailOutline } from "react-icons/md";
@@ -6,38 +6,69 @@ import { RiLockPasswordLine } from "react-icons/ri";
 import { MdPassword } from "react-icons/md";
 
 const Register = () => {
+
+  let [formData,setformData]=useState({})
+  let [RepeatPassword,setRepeatPassword] =useState("")
+  let handleChange=(e)=>{
+    let {name,value} =e.target
+    setformData({...formData,[name]:value})
+
+  }
+  let checkPassword=(e)=>{
+
+   let{value}= e.target
+   setRepeatPassword(value)
+   if(RepeatPassword ==formData.password)
+   {
+    return true
+   }
+   else{
+    return false
+   }
+  }
+  let handleSubmit=(e)=>{
+e.preventDefault()
+console.log(formData);
+ 
+  }
+
+
+
+
+  
+
   return (
 
     <div className='bg-[#efefef] size-full flex justify-center items-center '>
-      <form action="" className='w-1/2 h-3/4 rounded-3xl shadow-2xl flex justify-center items-center flex-col
-      gap-5 px-[80px] '> 
+      <form action="" className='w-1/2 h-[80%] rounded-3xl shadow-2xl flex justify-center items-center flex-col
+      gap-7 px-[60px]  max-sm:w-[90%] overflow-auto ' onSubmit={handleSubmit}> 
      <div className=' w-full flex justify-center items-center'>
-      <h1 className='font-bold text-3xl'>Registration Form</h1>
+      <h1 className='font-bold text-3xl   '>Registration Form</h1>
      </div>
      <div className=' w-full flex justify-center items-center  border-2 rounded-sm px-4 py-1.5'>
-      <input type="text" name='' placeholder='Enter Name' className='w-full outline-0'/>
+      <input type="text" name='name' placeholder='Enter Name' className='w-full outline-0' onChange={handleChange}/>
       <span><CgNametag /></span>
      </div>
       
       <div className=' w-full flex justify-center items-center  border-2 rounded-sm px-4 py-1.5' >
-      <input type="text" name='' placeholder='Enter username' className='w-full outline-0'/>
+      <input type="text" name='userName' placeholder='Enter username' className='w-full outline-0' onChange={handleChange }/>
       <span><FaRegUser /></span>
      </div>
      <div className=' w-full flex justify-center items-center  border-2 rounded-sm px-4 py-1.5'>
-      <input type="email" name=''  placeholder='Enter email' className='w-full outline-0'/>
+      <input type="email" name='email'  placeholder='Enter email' className='w-full outline-0'onChange={handleChange}/>
       <span><MdOutlineMailOutline /></span>
      </div>
      <div className=' w-full flex justify-center items-center  border-2 rounded-sm px-4 py-1.5'>
-      <input type="password" name='' placeholder='Enter password' className='w-full outline-0'/>
+      <input type="password" name='password' placeholder='Enter password' className='w-full outline-0' onChange={handleChange}/>
       <span><RiLockPasswordLine /></span>
      </div>
      <div className=' w-full flex justify-center items-center  border-2 rounded-sm px-4 py-1.5'>
-      <input type="password" name='' placeholder='Re-Type password' className='w-full outline-0'/>
+      <input type="password" name='Repeatpassword' placeholder='Re-Type password' className='w-full outline-0' onChange={checkPassword} />
       <span><MdPassword /></span>
      </div>
 
      <div className=' w-full flex justify-center items-center  border-2 rounded-sm  bg-black '>
-      <button className='h-10 text-white font-bold text-md'>Click</button>
+      <button className='h-10 w-full text-white font-bold text-md'>Click</button>
      </div>
       </form>
     </div>
